d30c6cd6af65004f4e91fd2f6f90d88a50c80498
inseok1121 <inseck1121@gmail.com>
2019-01-19 15:46:54 +0900

    update chapter 1 from songp

---
+++ 1.the_computation_graph/2_device.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+with tf.Session() as sess:
+        with tf.device("/cpu:0"):
+
+                """
+                cpu : /cpu:0
+                gpu1 : /gpu:0
+                gpu2 : /gpu:1
+                """
+
+                matrix1 = tf.constant([[3., 3.]])
+                matrix2 = tf.constant([[2.],[2.]])
+                
+                product = tf.matmul(matrix1, matrix2)
+                result = sess.run(product)
+                
+                print(result)
+
